6faa4c8676d570713318040efe2aec7790aa8268
Izhar Shaikh <izharits@gmail.com>
2017-02-17 20:17:06 -0500

    Fixed a bug in the generate_output.py script.

--- test_files/generate_output.py
+++ test_files/generate_output.py
@@ -3,18 +3,19 @@
 # @Email:  izharits@gmail.com
 # @Filename: test_output.py
 # @Last modified by:   Izhar Shaikh
-# @Last modified time: 2017-02-15T21:27:55-05:00
+# @Last modified time: 2017-02-17T19:25:35-05:00
 
 import sys
 
 fileToOpen = open(sys.argv[1], "rb")
-output = open(str(sys.argv[1] + "_output" ), "wb")
 
 line = ' '
 accountList = dict()
+accountListInOrder = []
 fromAccount = 0
 toAccount = 0
 transferAmount = 0
+count = 0
 
 while True:
     line = fileToOpen.readline()
@@ -23,23 +24,17 @@ while True:
     if line[0] != 'T':
         account, balance = (int(i) for i in line.split())
         accountList[account] = balance
+        count = count + 1
+        accountListInOrder.append(account)
         #print account, balance
     else:
-        #print line
         _, fr, to, tr = line.split()
         fromAccount, toAccount, transferAmount = int(fr), int(to), int(tr)
-        #print fromAccount, toAccount, transferAmount
         accountList[fromAccount] -= transferAmount
         accountList[toAccount] += transferAmount
+        #print fromAccount, toAccount, transferAmount
 
-#print accountList
-
-outputString = ""
-
-for k, v in accountList.items():
-    outputString += (str(k) + " " + str(v) + "\n")
-
-output.write(outputString)
+for account in accountListInOrder:
+    print account, accountList[account]
 
 fileToOpen.close()
-output.close()
